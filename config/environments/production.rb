db182b4649ec2c527aed9f438b8579394c741b63
Duc Tran <minhductran.ntt@gmail.com>
2019-08-20 00:45:18 -0500

    fix deploying

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "rails-spotify-songs-group_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.frameworks -= [ :active_record]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
