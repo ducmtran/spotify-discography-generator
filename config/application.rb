a1528b1584f380c17dd5119c9831072b86583e9d
Duc Tran <minhductran.ntt@gmail.com>
2019-08-20 00:51:15 -0500

    fix deploying

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,15 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "active_model/railtie" 
+# And now the rest
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "active_job/railtie" # Only for Rails >= 4.2
+require "action_cable/engine" # Only for Rails >= 5.0
+require "active_storage/engine" # Only for Rails >= 5.2
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
